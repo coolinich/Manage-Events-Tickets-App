a845d1c089eb6ed8fe5b925fdc131d5b396c5d6a
coolinich <coolinich@gmail.com>
2021-09-24 14:03:14 +0300

    BE: another implementation of repository to store data, also calculation for event capacity

--- Backend/src/repositories-impl/infile-events-repository.js
+++ Backend/src/repositories-impl/infile-events-repository.js
@@ -1,15 +1,26 @@
 import EventsRepository from '../business-logic/events-repository.js';
 import { TICKET_STATUSES } from '../../models/constants.js';
 import { generateID } from '../utils.js';
+import * as fs from'fs';
 
-export default class InMemoryEventsRepository extends EventsRepository {
+export default class InFileEventsRepository extends EventsRepository {
+    fileAsDB = 'assets/events.json';
     constructor() {
         super();
-        this.events = [];
+        let getExistingEvents = fs.readFileSync(this.fileAsDB);
+        this.events = JSON.parse(getExistingEvents);
+    }
+
+    async saveTofileAsDB(dataToSave) {
+        const formattedData = JSON.stringify(dataToSave, null, 2)
+        fs.writeFile(this.fileAsDB, formattedData, (err) => {
+            if (err) throw err;
+        });
     }
 
     async addEvent(newEvent) {
         this.events.push(newEvent);
+        await this.saveTofileAsDB(this.events);
     }
 
     async getAllEvents() {
@@ -27,7 +38,6 @@ export default class InMemoryEventsRepository extends EventsRepository {
                     validated: ticketsPerEvent.filter(ticket => ticket.status === TICKET_STATUSES[1]).length
                 }
             }
-
         });
     }
 
@@ -36,8 +46,8 @@ export default class InMemoryEventsRepository extends EventsRepository {
     }
 
     async addTicketsToEvent(eventId, listOfTickets) {
-        const findEvent = this.events.find(eventToAddTickets => eventToAddTickets.id === eventId);
-        const ticketsPerEvent = findEvent ? findEvent.tickets : null;
+        const eventToAddTickets = this.events.find(eventToAddTickets => eventToAddTickets.id === eventId);
+        const ticketsPerEvent = eventToAddTickets ? eventToAddTickets.tickets : [];
 
         const responseResult = {
             addedTickets: [],
@@ -45,7 +55,7 @@ export default class InMemoryEventsRepository extends EventsRepository {
         }
 
 
-        if (!ticketsPerEvent || !eventId || !listOfTickets) { 
+        if (!eventToAddTickets || !eventId || !listOfTickets) { 
             return responseResult;
         }
 
@@ -63,10 +73,25 @@ export default class InMemoryEventsRepository extends EventsRepository {
 
         let duplicatedRecords = listOfTickets
         .filter(newTicket => ticketsPerEvent.some(existingTicket => newTicket.validationCode === existingTicket.validationCode)); 
-
-        ticketsPerEvent.push(...ticketsToAdd);
-        responseResult['addedTickets'] = ticketsToAdd;
         responseResult['rejectedTickets'] = duplicatedRecords;
+        
+        if (ticketsToAdd.length) {
+            if (eventToAddTickets && eventToAddTickets.capacity) {
+                const availableCapacity = eventToAddTickets.capacity - ticketsPerEvent.length;
+                const possibleToAdd = ticketsToAdd.slice(0, availableCapacity);
+                ticketsPerEvent.push(...possibleToAdd);
+                responseResult['addedTickets'] = possibleToAdd;
+                responseResult['rejectedTickets'].push(...ticketsToAdd.slice(availableCapacity));
+            } else {
+                ticketsPerEvent.push(...ticketsToAdd);
+                responseResult['addedTickets'] = ticketsToAdd;
+            }
+            
+            eventToAddTickets.tickets = ticketsPerEvent;
+            await this.saveTofileAsDB(this.events);
+        }
+        
+
         return responseResult;
     }
 
@@ -84,6 +109,7 @@ export default class InMemoryEventsRepository extends EventsRepository {
         
         if (ticketToValidate['status'] === TICKET_STATUSES[0]) {
             ticketToValidate['status'] = TICKET_STATUSES[1];
+            await this.saveTofileAsDB(this.events);
             return {
                 result: true,
                 details: 'Ticket validated'
@@ -95,5 +121,4 @@ export default class InMemoryEventsRepository extends EventsRepository {
             };
         }
     }
-
 }
